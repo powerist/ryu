2f0ae37526793dd0511216d1ef45fedd5d98d821
Changbin Liu <changbin.liu@gmail.com>
2014-02-07 00:11:16 -0500

    Fix coding style according to flake8

--- ryu/app/inception_dhcp.py
+++ ryu/app/inception_dhcp.py
@@ -1,21 +1,34 @@
-"""
-Inception Cloud DHCP module
-"""
+# -*- coding: utf-8 -*-
+
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#    Copyright (C) 2014 AT&T Labs All Rights Reserved.
+#    Copyright (C) 2014 University of Pennsylvania All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 import logging
 import os
 
-from ryu.ofproto import ether
-from ryu.ofproto import inet
+from ryu.app import inception_conf as i_conf
+from ryu.app import inception_util as i_util
 from ryu.lib.dpid import str_to_dpid
-from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ipv4
+from ryu.lib.packet import packet
 from ryu.lib.packet import udp
-from ryu.app.inception_util import zk_data_to_tuple
-from ryu.app.inception_conf import MAC_TO_DPID_PORT
-from ryu.app.inception_conf import DHCP_SWITCH_DPID
-from ryu.app.inception_conf import DHCP_SWITCH_PORT
+from ryu.ofproto import ether
+from ryu.ofproto import inet
 
 LOGGER = logging.getLogger(__name__)
 
@@ -24,20 +37,18 @@ DHCP_CLIENT_PORT = 68
 
 
 class InceptionDhcp(object):
-    """
-    Inception Cloud DHCP module for handling DHCP packets
-    """
+    """Inception Cloud DHCP module for handling DHCP packets."""
 
     def __init__(self, inception):
         self.inception = inception
 
     def update_server(self, dpid, port):
-        dhcp_switch_dpid, _ = self.inception.zk.get(DHCP_SWITCH_DPID)
-        dhcp_switch_port, _ = self.inception.zk.get(DHCP_SWITCH_PORT)
+        dhcp_switch_dpid, _ = self.inception.zk.get(i_conf.DHCP_SWITCH_DPID)
+        dhcp_switch_port, _ = self.inception.zk.get(i_conf.DHCP_SWITCH_PORT)
         if dhcp_switch_port and dhcp_switch_dpid:
             LOGGER.warning("DHCP-server-connected switch registered before!")
-        self.inception.zk.set(DHCP_SWITCH_DPID, dpid)
-        self.inception.zk.set(DHCP_SWITCH_PORT, port)
+        self.inception.zk.set(i_conf.DHCP_SWITCH_DPID, dpid)
+        self.inception.zk.set(i_conf.DHCP_SWITCH_PORT, port)
 
     def handle(self, event):
         # process only if it is DHCP packet
@@ -58,8 +69,8 @@ class InceptionDhcp(object):
                          DHCP_SERVER_PORT)
             return
 
-        dhcp_switch_dpid, _ = self.inception.zk.get(DHCP_SWITCH_DPID)
-        dhcp_switch_port, _ = self.inception.zk.get(DHCP_SWITCH_PORT)
+        dhcp_switch_dpid, _ = self.inception.zk.get(i_conf.DHCP_SWITCH_DPID)
+        dhcp_switch_port, _ = self.inception.zk.get(i_conf.DHCP_SWITCH_PORT)
 
         LOGGER.info("Handle DHCP packet")
         if not dhcp_switch_dpid or not dhcp_switch_port:
@@ -86,8 +97,8 @@ class InceptionDhcp(object):
         # the client and forward the packet to it.
         elif udp_header.src_port == DHCP_SERVER_PORT:
             dpid_port, _ = self.inception.zk.get(os.path.join(
-                MAC_TO_DPID_PORT, ethernet_header.dst))
-            dpid, port = zk_data_to_tuple(dpid_port)
+                i_conf.MAC_TO_DPID_PORT, ethernet_header.dst))
+            dpid, port = i_util.str_to_tuple(dpid_port)
             LOGGER.info("Forward DHCP message to client (mac=%s) at "
                         "(switch=%s) (port=%s)", ethernet_header.dst,
                         dpid, port)
--- ryu/app/inception_util.py
+++ ryu/app/inception_util.py
@@ -1,19 +1,34 @@
-"""
-Inception utilities
-"""
+# -*- coding: utf-8 -*-
+
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#    Copyright (C) 2014 AT&T Labs All Rights Reserved.
+#    Copyright (C) 2014 University of Pennsylvania All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Inception utilities"""
 
 
-def tuple_to_zk_data(data_tuple, sep=','):
-    """
-    Convert tuple to string
-    """
+def tuple_to_str(data_tuple, sep=','):
+    """Convert tuple to string."""
+
     zk_data = sep.join(data_tuple)
     return zk_data
 
 
-def zk_data_to_tuple(zk_data, sep=','):
-    """
-    Convert string to tuple
-    """
+def str_to_tuple(zk_data, sep=','):
+    """Convert string to tuple."""
+
     data_tuple = tuple(zk_data.split(sep))
     return data_tuple
